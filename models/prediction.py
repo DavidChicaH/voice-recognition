d605d66a78a19023dd923efe9cc281e19a9e78dc
David Chica <david.chikhb@gmail.com>
2024-11-15 11:00:43 -0500

    New Graph
    New graph that shows where's the new audio features located

--- models/prediction.py
+++ models/prediction.py
@@ -2,29 +2,74 @@ import joblib
 import pandas as pd
 import numpy as np
 from scripts.extract_and_test_features import extract_features_for_prediction
+from matplotlib import pyplot as plt
+import seaborn as sns
+
 
 def predict():
-    # Extraer características
-    features = extract_features_for_prediction('./data/cleaned/prediction_audio_test.wav')
     
-    # Convertir a DataFrame
+    features = extract_features_for_prediction('./data/cleaned/prediction_audio_test.wav')
     features_df = pd.DataFrame(features)
 
-    # Cargar el scaler
+    
     scaler = joblib.load('./models/scaler.pkl')
     features_normalized = scaler.transform(features_df)
 
-    # Cargar el modelo
+    
+    pca = joblib.load('./models/pca_model.pkl')
+    new_audio_pca = pca.transform(features_normalized)
+
+    
     model = joblib.load('./models/svm_model.pkl')
 
-    # Obtener la predicción
+    
     prediction = model.predict(features_normalized)
     probabilities = model.predict_proba(features_normalized)
 
-    # Umbral de confianza
+    
     max_prob = np.max(probabilities)
     if max_prob >= 0.6:
         user_id = prediction[0]
         print(f"ID de usuario predicho: {user_id} (Confianza: {max_prob*100:.2f}%)")
     else:
         print("La voz no coincide con ninguno de los usuarios registrados.")
+
+    data = pd.read_csv('./data/csv/final_audio_features.csv')
+    X = data.iloc[:, 1:]  # Features
+    y = data.iloc[:, 0]  # Target / Id
+
+    X_normalized = scaler.transform(X)
+    X_pca = pca.transform(X_normalized)
+
+
+    pca_df = pd.DataFrame(X_pca, columns=['PCA1', 'PCA2'])
+    pca_df['user_id'] = y
+
+
+    plt.figure(figsize=(10, 6))
+    scatter = sns.scatterplot(
+        x='PCA1',
+        y='PCA2',
+        hue='user_id',
+        data=pca_df,
+        palette="Set1",
+        s=100,
+        marker='o',
+        legend='full'
+    )
+
+    plt.scatter(
+        new_audio_pca[0, 0],
+        new_audio_pca[0, 1],
+        c='black',
+        s=200,
+        marker='X',
+        label="Nuevo Audio"
+    )
+
+    plt.title("Distribución de características de la voz (PCA 2D) con nuevo audio", fontsize=16)
+    plt.xlabel("Componente Principal 1")
+    plt.ylabel("Componente Principal 2")
+    plt.legend(title="Datos")
+    plt.grid(True)
+    plt.show()
--- models/train_model.py
+++ models/train_model.py
@@ -11,35 +11,15 @@ def train_model():
     print("Training model")
     data = pd.read_csv('./data/csv/final_audio_features.csv')
 
-    X = data.iloc[:, 1:] # Features
-    y = data.iloc[:, 0] # Target / Id
+    X = data.iloc[:, 1:]  # Features
+    y = data.iloc[:, 0]  # Target / Id
 
     scaler = StandardScaler()
-
     X_normalized = scaler.fit_transform(X)
-
     joblib.dump(scaler, './models/scaler.pkl')
-    
-    X_train, X_test, Y_train, Y_test = train_test_split(X_normalized, y, test_size=0.2, random_state=42, stratify=y )
-    
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X_normalized, y, test_size=0.2, random_state=42, stratify=y
+    )
     print("Model trained")
-    
-    pca = PCA(n_components=2)
-    X_pca = pca.fit_transform(X)
-
-    # Crear un DataFrame para los resultados de PCA
-    pca_df = pd.DataFrame(X_pca, columns=['PCA1', 'PCA2'])
-    pca_df['user_id'] = y
-
-    # Graficar los resultados con diferentes colores para cada ID
-    plt.figure(figsize=(10, 6))
-    sns.scatterplot(x='PCA1', y='PCA2', hue='user_id', data=pca_df, palette="Set1", s=100, marker='o')
-    plt.title("Distribución de características de la voz (PCA 2D)", fontsize=16)
-    plt.xlabel("Componente Principal 1")
-    plt.ylabel("Componente Principal 2")
-    plt.legend(title="ID Usuario")
-    plt.grid(True)
-    plt.show()
-    
     return X_train, X_test, Y_train, Y_test
-
